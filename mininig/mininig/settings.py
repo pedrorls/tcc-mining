bc90ca64b7a9c180f644271984d0c8712d942f43
Pedro Silva <pedro.rls93@gmail.com>
2019-09-10 20:30:44 -0300

    add config

--- mininig/mininig/settings.py
+++ mininig/mininig/settings.py
@@ -11,20 +11,22 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from unipath import Path
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_DIR = Path(__file__).parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "jshw05@4y$4k@xep#nz9sq%sc_7w##_p$!y6=b@w+ph7(g3%d+"
+SECRET_KEY = config("SECRET_KEY")
+DEBUG = config("DEBUG", default=False, cast=bool)
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
@@ -55,7 +57,7 @@ ROOT_URLCONF = "mininig.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [PROJECT_DIR.child("templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -112,4 +114,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = PROJECT_DIR.parent.parent.child("static")
 STATIC_URL = "/static/"
+
+STATIC_FILES_DIRS = (PROJECT_DIR.child("static"),)
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = PROJECT_DIR.parent.parent.child("media")
